5e922071dbf263b7d0af59e05ade21467824090a
NoName1236 <2324494322@qq.com>
2021-08-04 21:40:21 +0800

    commit in August 4 21:39

--- app/src/main/java/com/hui/acyivitytest/MainActivity.java
+++ app/src/main/java/com/hui/acyivitytest/MainActivity.java
@@ -1,14 +1,65 @@
 package com.hui.acyivitytest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button_1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "you clicked button 1", Toast.LENGTH_SHORT).show();
+//                finish();
+//                Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+//                startActivity(intent);
+//                Intent intent=new Intent("com.hui.acyivitytest.ACTION_START");
+//                intent.addCategory("com.hui.acyivitytest.MY_CATEGORY");
+//                startActivity(intent);
+                Intent intent=new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.bilibili.com"));
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(MainActivity.this,"Add",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(MainActivity.this,"Remove",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        Toast.makeText(MainActivity.this,TAG+"pressed back key",Toast.LENGTH_SHORT).show();
+        super.onBackPressed();
     }
 }
---
+++ app/src/main/java/com/hui/acyivitytest/SecondActivity.java
@@ -0,0 +1,24 @@
+package com.hui.acyivitytest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+        Button button=findViewById(R.id.button_2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
