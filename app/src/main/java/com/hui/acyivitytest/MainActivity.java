3c02e3dd09b12f4e81d1c74c76709be96c3fe863
NoName1236 <2324494322@qq.com>
2021-08-05 08:02:35 +0800

    commit in August 5 08:02

--- app/src/main/java/com/hui/acyivitytest/MainActivity.java
+++ app/src/main/java/com/hui/acyivitytest/MainActivity.java
@@ -1,6 +1,7 @@
 package com.hui.acyivitytest;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -15,6 +16,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+    public static final int REQUEST_CODE=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,9 +32,18 @@ public class MainActivity extends AppCompatActivity {
 //                Intent intent=new Intent("com.hui.acyivitytest.ACTION_START");
 //                intent.addCategory("com.hui.acyivitytest.MY_CATEGORY");
 //                startActivity(intent);
-                Intent intent=new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse("https://www.bilibili.com"));
-                startActivity(intent);
+//                Intent intent=new Intent(Intent.ACTION_VIEW);
+//                intent.setData(Uri.parse("https://www.bilibili.com"));
+//                startActivity(intent);
+//                Intent intent=new Intent(Intent.ACTION_DIAL);
+//                intent.setData(Uri.parse("tel:10086"));
+//                startActivity(intent);
+                //Intent向下一个活动传递数据
+                String data="Hello SecondActivity!";
+                Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+                intent.putExtra("extra_data",data);//键值
+//                startActivity(intent);
+                startActivityForResult(intent,REQUEST_CODE);
             }
         });
     }
@@ -61,5 +72,16 @@ public class MainActivity extends AppCompatActivity {
     public void onBackPressed() {
         Toast.makeText(MainActivity.this,TAG+"pressed back key",Toast.LENGTH_SHORT).show();
         super.onBackPressed();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                String returned_data = data.getStringExtra("data_return");
+                Toast.makeText(MainActivity.this, returned_data, Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
--- app/src/main/java/com/hui/acyivitytest/SecondActivity.java
+++ app/src/main/java/com/hui/acyivitytest/SecondActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -14,11 +15,23 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_layout);
         Button button=findViewById(R.id.button_2);
+        final Intent intent=getIntent();
+        final String data= intent.getStringExtra("extra_data");
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Toast.makeText(SecondActivity.this,data,Toast.LENGTH_SHORT).show();
+                Intent intent1=new Intent();
+                intent1.putExtra("data_return","Hello FirstActivity!");
+                setResult(RESULT_OK,intent1);
+                finish();
             }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        Toast.makeText(SecondActivity.this,"保存成功！",Toast.LENGTH_SHORT).show();
+        super.onBackPressed();
     }
 }
